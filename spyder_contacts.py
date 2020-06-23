11d749121dddbb323d8a1af0a3e69a5e191284e4
JulixQuid <30515622+JulixQuid@users.noreply.github.com>
2020-06-22 23:51:27 -0500

    headers ready

--- spyder_contacts.py
+++ spyder_contacts.py
@@ -2,3 +2,23 @@
 Spyder contacts
 
 '''
+import selenium
+import pandas as pd
+
+
+class SpyderPuti():
+    self.total=0
+    self.dataframe=pd.DataFrame()
+
+    def __init__(self):
+        self.driver = webdriver.Firefox()
+
+    def begin(self):
+
+
+    def extract_contacts(self):_
+
+
+    def load_more(self):
+
+
